815fb086832797e938a26f2174b15559520ab3a1
melanytorrez <torrezmelany13@gmail.com>
2025-02-24 23:05:52 -0400

    feat: convertir 1 a I

---
+++ src/romanos.js
@@ -0,0 +1,24 @@
+// archivo: romanos.js
+function convertirARomanos(num) {
+    const valores = [
+        // { valor: 500, simbolo: 'D' },
+        // { valor: 100, simbolo: 'C' },
+        // { valor: 50, simbolo: 'L' },
+        // { valor: 10, simbolo: 'X' },
+        // { valor: 5, simbolo: 'V' },
+        { valor: 1, simbolo: 'I' }
+    ];
+
+    let resultado = '';
+    for (let i = 0; i < valores.length; i++) {
+        while (num >= valores[i].valor) {
+            resultado += valores[i].simbolo;
+            num -= valores[i].valor;
+        }
+    }
+
+    return resultado;
+}
+
+module.exports = convertirARomanos;
+export default convertirARomanos;
---
+++ src/romanos.spec.js
@@ -0,0 +1,26 @@
+// archivo: romanos.test.js
+//const convertirARomanos = require('./romanos');
+import convertirARomanos from"./romanos.js"
+test('Convierte 1 a I', () => {
+    expect(convertirARomanos(1)).toBe('I');
+});
+
+// test('Convierte 5 a V', () => {
+//     expect(convertirARomanos(5)).toBe('V');
+// });
+
+// test('Convierte 10 a X', () => {
+//     expect(convertirARomanos(10)).toBe('X');
+// });
+
+// test('Convierte 50 a L', () => {
+//     expect(convertirARomanos(50)).toBe('L');
+// });
+
+// test('Convierte 100 a C', () => {
+//     expect(convertirARomanos(100)).toBe('C');
+// });
+
+// test('Convierte 500 a D', () => {
+//     expect(convertirARomanos(500)).toBe('D');
+// });
